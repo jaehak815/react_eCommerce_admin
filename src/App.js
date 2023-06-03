83fd1a392486e2e892a106e24f0b602c9f65cbcc
Jay <rright815@gmail.com>
2023-06-03 22:24:16 +1000

    data_grid_product_list

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
 
 function App() {
   return (
@@ -22,6 +23,9 @@ function App() {
         <Route path="/users" element={<UserList />} />
         <Route path="/user/:userId" element={<User />} />
         <Route path="/newUser" element={<NewUser />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/product/:productId" element={<User />} />
+        <Route path="/newProduct" element={<NewUser />} />
         </Routes>
       </div>
     </Router>
