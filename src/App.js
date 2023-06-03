3af0ec84ad9905a7a3281f98798c2eea182e2682
Jay <rright815@gmail.com>
2023-06-03 22:02:14 +1000

    add_newUserPage

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route path="/users" element={<UserList />} />
         <Route path="/user/:userId" element={<User />} />
+        <Route path="/newUser" element={<NewUser />} />
         </Routes>
       </div>
     </Router>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,6 +11,7 @@ import {LineStyle,
     ChatBubbleOutline,
     WorkOutline,
     Report,} from '@material-ui/icons';
+import { Link } from "react-router-dom";
 
 export default function Sidebar() {
   return (
@@ -36,14 +37,18 @@ export default function Sidebar() {
             <div className="sidebarMenu">
                 <h3 className="sidebarTitle">Quick Menu</h3>
                 <ul className="sidebarList">
+                    <Link to="/users" className="link">
                     <li className="sidebarListItem ">
                     <PermIdentity className="sidebarIcon"/>
                     Users
                     </li>
+                    </Link>
+                    <Link to="/products" className="link">
                     <li className="sidebarListItem">
                     <Storefront className="sidebarIcon"/>
                     Products
                     </li>
+                    </Link>
                     <li className="sidebarListItem">
                     <AttachMoney className="sidebarIcon"/>
                     Transactions
