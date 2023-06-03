d7d18d7eb061aad074dda552fded1ebea8cdbfdb
Jay <rright815@gmail.com>
2023-06-03 18:06:18 +1000

    usersPage_deleteFunction_useState

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css";
 import Home from "./pages/home/Home";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Topbar/>
       <div className="container">
         <Sidebar />
-      <Home/>
+        <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/users" element={<UserList />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
