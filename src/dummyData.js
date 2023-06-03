e48d25de356870edff85d90c6dcdf733e73637a3
Jay <rright815@gmail.com>
2023-06-03 23:25:34 +1000

    productPage_newProductPage

--- src/dummyData.js
+++ src/dummyData.js
@@ -50,6 +50,22 @@ export const userData = [
     
   ];
 
+  export const productData = [
+    {
+      name: "Jan",
+      "Sales": 4000,
+    },
+    {
+      name: "Feb",
+      "Sales": 3000,
+    },
+    {
+      name: "Mar",
+      "Sales": 5000,
+    },
+  ];
+
+
   export const userRows = [
     { id: 1, username: 'Jon Snow', avatar: "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     email: "jon@gmail.com",
